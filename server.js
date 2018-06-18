a8f48b2a6d9c09c6485a14bb59ad31b2d04a5955
Artur Veselovski <artur.veselovski@hotmail.fi>
2018-06-18 22:14:26 +0300

    test

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use((req, res, next) => {
+	if (req.header('x-forwarded-proto') !== 'https') {
+		res.redirect(`https://${req.header('host')}${req.url}`);
+	} else next();
+});
 app.use(express.static('build'));
-
 app.get('*', function(req, res) {
 	res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
